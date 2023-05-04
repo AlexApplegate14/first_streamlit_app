70cc3f3655ac1d2616b66ce3eb804232d127c6f3
AlexApplegate14 <132586449+AlexApplegate14@users.noreply.github.com>
2023-05-04 10:05:18 -0500

    Update streamlit_app.py
    added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,6 @@ sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 sl.dataframe(my_fruit_list)
+
+sl.multiselect("Pick some fruits:", list(my_fruit_list.index))
+sl.dataframe(my_fruit_List)
