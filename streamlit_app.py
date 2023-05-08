48d7631532b75fd7880eaf9a3bbdf43fe5081d2a
AlexApplegate14 <132586449+AlexApplegate14@users.noreply.github.com>
2023-05-08 11:32:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
   fruit_choice = sl.text_input('What fruit would you like information about?')
   if not fruit_choice:
     sl.error('Please select a fruit to get information.')
-  else:
+else:
  back_from_function = get_fruityvice_data(fruit_choice)
  sl.dataframe(back_from_function)
 except URLError as e:
