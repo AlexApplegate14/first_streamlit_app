f40e39843e5805e2887f06b4c2f7c0085d257d14
AlexApplegate14 <132586449+AlexApplegate14@users.noreply.github.com>
2023-05-08 11:31:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_selected = sl.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 sl.dataframe(fruits_to_show)
 
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     sl.dataframe(fruityvice_normalized)
